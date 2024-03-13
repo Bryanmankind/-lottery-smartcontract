bbbb2e2b3f6d09fa6d6e5060bd19ab2b95e0e93a
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-13 17:33:22 +0100

    smart contract

--- scripts/lotterContract.sol
+++ scripts/lotterContract.sol
@@ -1,33 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// SPDX-License-Identifier: MIT;
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+pragma solidity 0.8.24;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+contract Lottery { 
+    address public owner;
+    address payable[] public players;
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    constructor() {
+        owner = msg.sender;
+    }
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    function enterLottery() public {
+        players.push(msg.sender);
+    }
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
