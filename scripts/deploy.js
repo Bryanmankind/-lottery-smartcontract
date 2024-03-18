eaed64ab01c03d2db627398705949e55f5196289
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-18 10:53:01 +0100

    deploy contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,13 +5,14 @@ const { ethers } = require("hardhat");
         const LotteryContract = await LotteryContractFactory.waitForDeployment();
         console.log("Contract deployed to:", await LotteryContract.getAddress());
 
-        // call the functions 
-
-        txn = await LotteryContract.enterLottery()
-
-        await txn.wait()
-        console.log("Player one entered with:", )
-    
+        
+       // Call the enterLottery function
+       const amountToSend = ethers.utils.parseEther("0.02"); // Sending 0.02 ether
+       const enterLotteryTx = await LotteryContract.enterLottery({ value: amountToSend });
+       
+       await enterLotteryTx.wait();
+       
+       console.log("Player entered the lottery with amount:", ethers.utils.formatEther(amountToSend));
       };
       
       const runMain = async () => {
